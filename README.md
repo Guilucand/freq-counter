fba018f7aa3cedaeb5a4e9d698a80f33ec70567e
Andrea Cracco <guilucand@gmail.com>
2019-11-28 17:26:20 +0200

    Add readme and ctrl+c catch

---
+++ README.md
@@ -0,0 +1,1 @@
+Basic ascii characters frequency counter
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,45 @@
 use structopt::StructOpt;
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read, stdin};
+use std::process::exit;
 
 #[derive(StructOpt, Debug)]
 struct Cli {
     files: Vec<String>
 }
 
+static mut CTRLC_PRESSED: bool = false;
+
 fn main() {
     let args = Cli::from_args();
 
     let mut freqs = [0usize; 256];
-    let mut buffer = [0; 1024 * 1024];
+    let mut buffer = vec![0; 1024 * 1024 * 32];
 
-    for file in args.files {
-        let mut fd = File::open(file).unwrap();
-        while let Ok(count) = fd.read(&mut buffer) {
-            if count == 0 {
+    let mut process = |stream: &mut dyn Read| {
+        while let Ok(count) = stream.read(buffer.as_mut_slice()) {
+            if count == 0 || unsafe { CTRLC_PRESSED } {
                 break;
             }
             for el in &buffer[0..count] {
                 freqs[*el as usize] += 1;
             }
         }
+    };
+
+    ctrlc::set_handler(|| unsafe { CTRLC_PRESSED = true } );
+
+    if args.files.len() > 0 {
+        for file in args.files {
+            let mut fd = File::open(file).unwrap();
+            process(&mut fd);
+            if unsafe { CTRLC_PRESSED } {
+                break;
+            }
+        }
+    }
+    else {
+        process(&mut stdin());
     }
 
     println!("[[Global chars occurrences]]");
